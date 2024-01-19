ad37b27cc150eded864bff7330d10e31e0fe4809
Vuong Nguyen <nguyenquangvuong99@gmail.com>
2024-01-19 18:46:09 +0700

    hihi

---
+++ src/letcode-practise-2024/PascalsTriangle.java
@@ -0,0 +1,19 @@
+
+/*
+ * give the index k, 
+ * print the index of pascal 
+ * k = 1, 
+ * 1, 1
+ * k = 2
+ * 1 2 1
+ * k = 3
+ * 1 3 3 1
+ * 
+ */
+
+public class PascalsTriangle {
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
